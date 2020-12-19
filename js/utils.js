19a74257958b3012a21a72be98c96311a9fcfb9d
minalen75 <kelly.whiting@gmail.com>
2020-12-18 20:04:31 -0700

    merged and added stuff

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,42 @@
-function getPwdLength(message) {
-    document.getElementById('pwdLength').textContent = message;
-    document.getElementById('pwdLength').
+
+
+// Watch for value of Special Character usage
+let spChar = document.getElementById("specChar");
+    spChar.addEventListener('change', function () {    
+        if (this.check) {
+            console.log("Checkbox is checked");
+        }
+        else {
+            console.log("Checkbox is NOT checked"); 
+        }
+});
+
+// Watch for the value of special number usage
+let spNumbers = document.getElementById("includeNumbers");
+    spNumbers.addEventListener('change', function() {
+        if (this.check){
+            console.log("Checkbox is checked");
+        }
+        else {
+            console.log("")
+        }
+    })
+
+// Generate the password
+var pwd1 = 0;
+
+//console.log(document.getElementById("pwdLength").value);
+
+
+function generatePWD(){
+    pwd1 = +document.getElementById("pwdLength").value;
+    alert(pwd1);
+
 }
+
+
+
+document.getElementById("generate").addEventListener("click", generatePWD);
+
+
+
