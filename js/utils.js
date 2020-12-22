1b107992084b8b2f2fd4a27770d268a928bf9abb
minalen75 <kelly.whiting@gmail.com>
2020-12-21 19:02:18 -0700

    Fixed up the code

--- js/utils.js
+++ js/utils.js
@@ -1,77 +1,117 @@
-// // Watch for value of Special Character usage
-// let spChar = document.getElementById("specChar");
-// spChar.addEventListener('change', function () {
-//     if (this.check) {
-//         console.log("Checkbox is checked");
-
-//     } else {
-//         console.log("Checkbox is NOT checked");
-
-//     }
-// });
-
-// // Watch for the value of special number usage
-// let spNumbers = document.getElementById("includeNumbers");
-// spNumbers.addEventListener('change', function () {
-//     if (this.check) {
-//         console.log("Checkbox is checked");
-
-//     } else {
-//         console.log("Checkbox is NOT checked")
-
-//     }
-// })
-
-// Generate the password
+var includeSpecChar = false;
+var includeNumbers = false;
+var includePrompts = false;
+var includeUpperLetters = false; 
 var pwd1 = 0;
-var collectionOfLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+var collectionOfLetters = "abcdefghijklmnopqrstuvwxyz";
+var collectionofUpperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var collectionOfNumbers = "0123456789"
 var specialsymbols = "!@#$%^&*"
 var generatedPassWord = "";
 var useSpecialChar = 1;
 var useNumbers = 1;
 var randomNumber;
 var password = "";
-var n; 
+var n;
 
 
-//console.log(document.getElementById("pwdLength").value);
-
-
-function generatePWD() {
-    pwd1 = +document.getElementById("pwdLength").value;
-    // alert(pwd1); 
-
-    // checks upper bounds and lower bounds of variable
-    if ((pwd1 < 8) || (pwd1 > 128)) {
-        alert("Password Values must exceed 8 or be less than 128")
-    } else {
-        for (var i = 0; i < pwd1; i++) {
-            randomNumber = collectionOfLetters[Math.floor(Math.random() * collectionOfLetters.length)]
-            password += randomNumber
+    
+    function generatePWD() {
+        var includeSpecChar = document.getElementById("specChar").checked; 
+        var includeNumbers = document.getElementById("includeNumbers").checked;
+        var includeUpperLetters = document.getElementById("UpperLetters").checked;
+        
+        pwd1 = +document.getElementById("pwdLength").value;
+        
+        // checks upper bounds and lower bounds of variable
+        if ((pwd1 < 8) || (pwd1 > 128)) {
+            alert("Password Values must exceed 8 or be less than 128")
+        }
+       
+        // check for collection of just lower case letters
+        if ((includeSpecChar === false) && (includeNumbers === false) && (includeUpperLetters === false)) {
+            
+            // collectionOfLetters = "abcdefghijklmnopqrstuvwxyz"; 
+            // CREATE THE PASSWORD 
+            for (var i = 0; i < pwd1; i++) {
+                randomNumber = collectionOfLetters[Math.floor(Math.random() * collectionOfLetters.length)]
+                password += randomNumber
+            }
+            document.getElementById("password").innerHTML = password;
+            password = "";
 
         }
 
+        //check for lower case and upper case letters
+        if (includeUpperLetters === true) {
+            
+            collectionOfLetters = collectionOfLetters +  collectionofUpperLetters;
+            // CREATE THE PASSWORD 
+            for (var i = 0; i < pwd1; i++) {
+                randomNumber = collectionOfLetters[Math.floor(Math.random() * collectionOfLetters.length)]
+                password += randomNumber
+            }
+            document.getElementById("password").innerHTML = password;
+            password = "";
+            collectionOfLetters = "abcdefghijklmnopqrstuvwxyz";
 
-        if (password.includes("!") || password.includes("@") || password.includes("#") || password.includes("%") || password.includes("^") || password.includes("&") || password.includes("*"))
-            { 
+        }
+
+        // Check for collection of numbers, spec characters and numbers create password if all are present
+        if ((includeSpecChar === true) && includeNumbers === true) {
+            collectionOfLetters = collectionOfLetters + specialsymbols;
+            collectionOfLetters = collectionOfLetters + collectionOfNumbers;
+            // CREATE THE PASSWORD 
+            for (var i = 0; i < pwd1; i++) {
+                randomNumber = collectionOfLetters[Math.floor(Math.random() * collectionOfLetters.length)]
+                password += randomNumber
+            }
+            includeSpecChar = false;
+            includeNumbers = false; 
+            collectionOfLetters = "abcdefghijklmnopqrstuvwxyz";
+            // CHECK PASSWORD REQUIREMENTS - SPECIAL CHARACTERS
+            if (password.includes("!") || password.includes("@") || password.includes("#") || password.includes("%") || password.includes("^") || password.includes("&") || password.includes("*")) {
                 document.getElementById("password").innerHTML = password;
                 password = "";
+            } else {
+                password = "";
+                document.getElementById("password").innerHTML = password;
             }
-        else {
-            password="";
-            document.getElementById("password").innerHTML = password; 
+            // ############################################################
+
         }
-        //alert(password);
+        // Check for collection of letters and spec char
+        if ((includeSpecChar === true) || (includeNumbers === true)) {
+            if (includeSpecChar === true) {
+                collectionOfLetters = collectionOfLetters + specialsymbols;
 
+            }
+            if (includeNumbers === true) {
+                collectionOfLetters = collectionOfLetters + collectionOfNumbers;
+
+            }
+            // CREATE THE PASSWORD
+            for (var i = 0; i < pwd1; i++) {
+                randomNumber = collectionOfLetters[Math.floor(Math.random() * collectionOfLetters.length)]
+                password += randomNumber
+            }
+            includeSpecChar = false;
+            includeNumbers = false; 
+            collectionOfLetters = "abcdefghijklmnopqrstuvwxyz";
+            // CHECK PASSWORD REQUIREMENTS
+            if (password.includes("!") || password.includes("@") || password.includes("#") || password.includes("%") || password.includes("^") || password.includes("&") ||
+                password.includes("*") || password.includes("1") || password.includes("2") || password.includes("3") || password.includes("4") || password.includes("5") ||
+                password.includes("6") || password.includes("7") || password.includes("8") || password.includes("9") || password.includes("0")) {
+                document.getElementById("password").innerHTML = password;
+                password = "";
+            } else {
+                password = "";
+                document.getElementById("password").innerHTML = password;
+            }
+            // ###############################################################################################
+
+        }
     }
-
-
-
-
-
-
-
-}
 
 
 
